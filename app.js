3835a08bcea4e858f6a2fccaf8e44bf1c2e69508
saurav kumar <sauravkumar91937@gmail.com>
2025-08-11 13:58:31 +0530

    ui url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://swathyaai.netlify.app"],
+    origin: ["http://localhost:5173", "https://imsmanag.netlify.app"],
     credentials: true,
   })
 );
